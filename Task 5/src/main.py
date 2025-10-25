971450b137bba79612b1fd2a6f42e7a46f292a55
caribbean444 <misha.bogatow@yandex.ru>
2025-10-26 00:03:17 +0300

    Before Task 5

---
+++ Task 5/src/main.py
@@ -0,0 +1,21 @@
+from fastapi import Body, FastAPI, Query
+import uvicorn
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotels
+from src.config import settings
+
+
+print(settings.DB_URL)
+
+app = FastAPI()
+
+app.include_router(router_hotels)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
